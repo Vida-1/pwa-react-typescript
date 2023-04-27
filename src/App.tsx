19fdffeb63ca3e2f5ebc6030266fca1adcb2a788
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-04-27 05:26:35 +0000

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Test: Look at us learning React PWAs!
         </a>
       </header>
     </div>
